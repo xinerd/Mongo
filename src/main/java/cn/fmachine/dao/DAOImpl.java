a6e5ee935a087378f7e018a4f4fc9c1886f77d83
xinerd <m.xinerd@gmail.com>
2016-04-16 19:01:28 -0400

    fixed bug: keyword is null

--- src/main/java/cn/fmachine/dao/DAOImpl.java
+++ src/main/java/cn/fmachine/dao/DAOImpl.java
@@ -7,10 +7,7 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static com.mongodb.client.model.Filters.eq;
 
@@ -23,7 +20,6 @@ import static com.mongodb.client.model.Filters.eq;
  */
 public class DAOImpl {
 
-    //    private MongoClient mongoClient = new MongoClient("129.63.16.136",27017);
     private MongoClient mongoClient;
     private MongoDatabase db;
 
@@ -36,6 +32,30 @@ public class DAOImpl {
         return mongoClient.getDatabase(dbName);
     }
 
+    public void insert(String collectionName, Document document) {
+        db.getCollection(collectionName).insertOne(document);
+    }
+
+    public void batchInsert(String collectionName, List<Document> documents) {
+        db.getCollection(collectionName).insertMany(documents);
+    }
+
+    /**
+     * Find all id and assigned project name
+     * <p/>
+     * 2.Using the new Document class
+     * collection.find().projection(new Document("username", true).append("lastname", true).append("firstname", true));
+     * <p/>
+     * 3.Using the new Projections builder
+     * collection.find().projection(Projections.include("username", "lastname", "firstname"));
+     *
+     * @param projectName For example: keywords or related_questions
+     * @return all records with _id and corresponding project value.
+     */
+    public FindIterable<Document> findWithProjection(String collectionName, String projectName) {
+        return db.getCollection(collectionName).find()
+                .projection(new BasicDBObject(projectName, true).append("_id", true));
+    }
 
     public void findByFieldName(String collectionName, String fieldName, String fieldValue) {
         FindIterable<Document> iterable = db.getCollection(collectionName).find(
@@ -55,45 +75,6 @@ public class DAOImpl {
         printDocuments(iterable);
     }
 
-    /**
-     * Find all id and assigned project name
-     * <p/>
-     * 2.Using the new Document class
-     * collection.find().projection(new Document("username", true).append("lastname", true).append("firstname", true));
-     * <p/>
-     * 3.Using the new Projections builder
-     * collection.find().projection(Projections.include("username", "lastname", "firstname"));
-     *
-     * @param projectName For example: keywords or related_questions
-     * @return all records with _id and corresponding project value.
-     */
-    public FindIterable<Document> findWithProjection(String collectionName, String projectName) {
-        return db.getCollection(collectionName).find()
-                .projection(new BasicDBObject(projectName, true).append("_id", true))
-                .limit(10);
-    }
-
-    public Map<String, List<String>> saveKeywords(final FindIterable<Document> iterable) {
-        final Map<String, List<String>> keywordsContainer = new HashMap<String, List<String>>();
-        iterable.forEach(new Block<Document>() {
-            public void apply(Document document) {
-                String id = document.get("_id").toString();// get _id
-                ArrayList<String> keywords = (ArrayList<String>) document.get("keywords");// get keywords
-                // put keyword and corresponding id to map
-                for (String keyword : keywords) {
-                    if (keywordsContainer.containsKey(keyword)) {
-                        keywordsContainer.get(keyword).add(id);
-                    } else {
-                        List<String> sourceList = new ArrayList<String>();
-                        sourceList.add(id);
-                        keywordsContainer.put(keyword, sourceList);
-                    }
-                }
-            }
-        });
-        return keywordsContainer;
-    }
-
     public void printDocuments(FindIterable<Document> iterable) {
         iterable.forEach(new Block<Document>() {
             public void apply(Document document) {
@@ -101,6 +82,4 @@ public class DAOImpl {
             }
         });
     }
-
-
 }
